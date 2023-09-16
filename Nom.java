4efe45e2af7456bc2e230a663cc9b12f28794a44
parvez <parvez37370@gmail.com>
2023-09-16 23:39:03 +0530

    this is my complex design

---
+++ Nom.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+class Nom
+{
+public static void main(String[] args)
+{
+Scanner in=new Scanner(System.in);
+int n;
+System.out.println("enter the number: n");
+n=in.nextInt();
+if(n%2==0)
+{
+System.out.println("enter the is even");
+}
+else
+{
+System.out.println("enter the number is odd");
+}
+}
+}
